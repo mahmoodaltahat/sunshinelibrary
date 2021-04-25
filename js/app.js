6d93fabaf547aba4718002abd433f748ea8135ab
mahmoodaltahat <ma7moodaltahat@gmail.com>
2021-04-25 20:50:30 +0300

    creaiting new web site

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+alert('welcome to TFED store');
+  document.write("<pre>");
+  let user_name = prompt('please enter your name?')
+  document.write("<span> welcom " + user_name + "</span>");
+  console.log('welcom ' + user_name);
+  document.write("<pre>");
+  
+  let age = prompt('please enter your age?')
+  alert('god bless you');
+  document.write("<span> we wish " + age + " years were very succsesful </span>");
+  document.write("<pre>");
+  console.log(user_name +'age is =' + age);
+  document.write("<pre>");
+
+  var your_book = prompt('please enter a book kind (scince , cultural )?')
+  alert('great choice');
+  document.write("<span> to find " + your_book + " books visit " + your_book + " books page </span>");
+  document.write("<pre>");
+
+  let number_books = prompt('please enter the number of books  you want to buy or borrow?')
+  alert('you will get a great discount');
+  document.write("<span>" + user_name + " want " + number_books +  " books </span>");
+  console.log(user_name + "want " + number_books + " books" );
